7be259888c4560e012b86f37475962fcc2980796
maere <mary.sweeney@gmail.com>
2018-08-05 17:01:21 -0400

    forgot brackets

--- app/routes/greeting.js
+++ app/routes/greeting.js
@@ -2,9 +2,9 @@ import Route from '@ember/routing/route';
 
 
 
-export default Ember.Route.extend(
-  model(params){
-    const escapedName = encodedURIComponent(params.name);
-    return Ember.$.getJSON(`/api/greeting?name=${escapedName}`);
-  }
-);
+export default Ember.Route.extend({
+    model(params){
+      const escapedName = encodedURIComponent(params.name);
+      return Ember.$.getJSON(`/api/greeting?name=${escapedName}`);
+    }
+  });
